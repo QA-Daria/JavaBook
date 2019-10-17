9769f78d3639232ef0a256a35f8a05bd9fc1ec89
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2019-10-17 19:58:08 +0300

    addMatimaticFun

---
+++ JavaBookStarted/src/main/java/Chapter3/MathimaticFunction.java
@@ -0,0 +1,14 @@
+package Chapter3;
+
+public class MathimaticFunction {
+    public static void main(String[] args) {
+        double x = 4;
+        int u = 4;
+        double y = Math.sqrt(u);
+        System.out.println(u);
+
+        double w = Math.pow(u, 0);
+        System.out.println(w);
+
+    }
+}
