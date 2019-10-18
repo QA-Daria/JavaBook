a04d7f0506b88f720265bc3ea5bb624206c50dc3
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2019-10-18 14:29:45 +0300

    add lesson - switch
    simple switch example

---
+++ JavaBookStarted/src/main/java/Chapter3/ForSwotch.java
@@ -0,0 +1,29 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ForSwotch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Select an option 1, 2, 3 or 4");
+
+        int choice = in.nextInt();
+
+        switch (choice){
+            case 1:{ System.out.println("Kotik 1");
+                break;}
+            case 2:{
+                System.out.println("Kotik 2");
+                break;
+            }
+            case 3:
+                System.out.println("Kotik 3");
+                break;
+            default:
+                System.out.println("Yps");
+                break;
+
+
+        }
+    }
+}
