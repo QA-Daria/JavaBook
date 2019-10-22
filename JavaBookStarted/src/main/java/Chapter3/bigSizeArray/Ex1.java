83c8c249d9eeb12c1aeff502a31f8af439b1d402
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2019-10-22 16:09:24 +0300

    edit bigMass

--- JavaBookStarted/src/main/java/Chapter3/bigSizeArray/Ex1.java
+++ JavaBookStarted/src/main/java/Chapter3/bigSizeArray/Ex1.java
@@ -5,14 +5,13 @@ public class Ex1 {
         int [][] balances = new int [5][5];
 
         for (int i = 0; i < balances.length; i++){
-            for (int j = 0; j < balances.length; j++){
-               balances[i][0] = 5;
-                balances[0][j] =  3;
+            for (int j = 0; j < balances[i].length; j++){
+                balances[i][j] = j+1;
             }
         }
 
-        for (int i = 0; i <2; i++){
-            for (int j = 0; j < 2; j++){
+        for (int i = 0; i <balances.length; i++){
+            for (int j = 0; j < balances[0].length; j++){
                 System.out.print(" " + balances[i][j] + " ");
 
             }
