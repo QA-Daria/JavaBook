cf0054c59ce2d5882b684dbc4a2fb458e2b2e4db
Дарья Борисова <d.borisova@MacBook-Pro-Dara.local>
2019-10-22 15:17:15 +0300

    add new methode for mass

---
+++ JavaBookStarted/src/main/java/Chapter3/Mass/LotteryDrawing.java
@@ -0,0 +1,37 @@
+package Chapter3.Mass;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LotteryDrawing {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many numbers do you need to draw? ");
+        int k = in.nextInt();
+
+        System.out.println("What is the highest number you can draw? ");
+        int n = in.nextInt();
+
+        int [] massNum = new int[n];
+
+        for (int i = 0; i < massNum.length; i++){
+            massNum[i] = i + 1;
+        }
+
+        int [] resultMass = new int[k];
+        for (int i = 0; i < resultMass.length; i++){
+            int r = (int)(Math.random()* n);
+
+            resultMass[i] = massNum[r];
+            massNum[r] = massNum[n-1];
+            n--;
+        }
+        Arrays.sort(resultMass);
+        System.out.println("Bet the following combination. It'll make you rich!");
+
+        for (int r : resultMass) System.out.println(r);
+
+    }
+}
---
+++ JavaBookStarted/src/main/java/Chapter3/Mass/MassThree.java
@@ -0,0 +1,20 @@
+package Chapter3.Mass;
+
+import java.util.Arrays;
+
+public class MassThree {
+
+    public static void main(String[] args) {
+
+        
+        int [] mass = {1, 2, 4};
+
+        Arrays.fill(mass, 4);
+
+        for (int i:mass){
+            System.out.println(i);
+        }
+
+
+    }
+}
